9084708fa5faf91644883a10989d439429d152bd
Himanshu Chopra <himanchops@gmail.com>
2020-07-03 17:00:03 +0530

    Added user details

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :mobile_number, presence: true, numericality: true, length: {is: 10}
+
+  def full_name
+  	"#{first_name} #{last_name}" if first_name || last_name
+  end
 end
