d42b3290f0646e1e7ce9b31d907180f8e7ee18f4
Himanshu Chopra <himanchops@gmail.com>
2020-07-03 22:54:49 +0530

    Avatar and Documents

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   validates :mobile_number, presence: true, numericality: true, length: {is: 10}
+  has_one_attached :avatar
+  has_many_attached :documents
 
   def full_name
   	"#{first_name} #{last_name}" if first_name || last_name
