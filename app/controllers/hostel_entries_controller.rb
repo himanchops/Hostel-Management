01fb274ab26aa56d8495b27db581cc5b0281da72
Himanshu Chopra <himanchops@gmail.com>
2020-07-15 05:17:16 +0530

    Edit, update, delete actions for hostel entry form

--- app/controllers/hostel_entries_controller.rb
+++ app/controllers/hostel_entries_controller.rb
@@ -1,24 +1,23 @@
 class HostelEntriesController < ApplicationController
   def create
-  	@user = User.find(params[:user_id])
-  	if @user.hostel_entries.empty?
-      @hostel_entry = @user.hostel_entries.build(hostel_entry_params) 
-      if @hostel_entry.save
-        flash[:notice] = "New hostel entry added successfully"
-      else
-        flash[:notice] = "Unsuccessful entry"
-      end
-    else
-      if @user.hostel_entries.update(hostel_entry_params)
-        flash[:notice] = "Hostel entry updated successfully"
-      else
-        flash[:alert] = "Unsuccessful entry"
-      end
-    end
-    redirect_to @user
+	@user = User.find(params[:user_id])
+	@hostel_entry = @user.hostel_entries.build(hostel_entry_params) 
+	if @hostel_entry.save
+		flash[:notice] = "New hostel entry added successfully"
+	else
+		flash[:notice] = "Unsuccessful entry"
+  	end
+  	redirect_to @user
   end
 
   def update
+  	@user = User.find(params[:user_id])
+	if @user.hostel_entries.update(hostel_entry_params)
+		flash[:notice] = "Hostel entry updated successfully"
+	else
+		flash[:alert] = "Unsuccessful entry"
+	end
+  	redirect_to @user	  	
   end
 
   def destroy
