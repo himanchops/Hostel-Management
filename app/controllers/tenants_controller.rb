c753cb0158ec3add1d2dde4c10b94f3efcf284d3
Himanshu Chopra <himanchops@gmail.com>
2020-08-01 12:35:15 +0530

    Tenant index listing all user filters

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -10,6 +10,7 @@ class TenantsController < ApplicationController
   	@users = User.with_attached_avatar.includes(:hostel_entries)
   	@current_users = User.with_attached_avatar.includes(:hostel_entries).where(hostel_entries: { vacated: false })
   	@vacated_users = User.with_attached_avatar.includes(:hostel_entries).where(hostel_entries: { vacated: true })
+    @due_rent_users = User.with_attached_avatar.includes(:hostel_entries).where.not( hostel_entries: { total_rent_due: 0})
   end
 
 end
